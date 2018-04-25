70ea1f390138eb6ecd93cd3669ee0e3dc3a43b65
alpag <pawlaq123@gmail.com>
2018-04-25 14:33:20 +0200

    initial

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+ 
+ return 0;
+}
